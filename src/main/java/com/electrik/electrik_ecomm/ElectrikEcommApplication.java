1eab4e6f057f48a42e4d9dcad9bc6c354e17e93a
“Tu <Acostadavid86@gmail.com>
2025-03-17 19:16:21 -0300

    first commit

---
+++ src/main/java/com/electrik/electrik_ecomm/ElectrikEcommApplication.java
@@ -0,0 +1,13 @@
+package com.electrik.electrik_ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrikEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrikEcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electrik/electrik_ecomm/ElectrikEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrik.electrik_ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrikEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
